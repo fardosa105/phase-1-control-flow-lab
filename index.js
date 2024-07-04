a231a8fbf98744bbc966971e313ade973e153272
+ <fadosa.abdirhman@student.moringaschool>
2024-07-05 00:57:41 +0300

    Completed JavaScript scope practice lab

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Declaring a global variable from inside a function
+}
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value';  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
